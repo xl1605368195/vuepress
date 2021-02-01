2e1731573d48d61e52bcade9732c11f7461c0aad
user01 <user01@gmail.com>
2021-02-01 09:20:01 +0800

    头部导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,45 @@
 module.exports = {
-    title: 'MyRasp官网', 
-    description: '全新一代JVM安全防护产品',
+    title: '文档CMS',
+    description: '文档管理中心',
+    dest: './dist',
+    port: '7777',
+    head: [
+        ['link', {rel: 'icon', href: '/logo.jpg'}]
+    ],
+    markdown: {
+        lineNumbers: true
+    },
     themeConfig: {
-        search: true,//搜索
-        searchMaxSuggestions: 10,
+        nav: require("./nav.js"),
+        sidebar: {'/guide/':[
+            {
+                  title:'新手指南',
+                  collapsable: true,
+                  children:[
+                    '/guide/notes/one',
+                  ]
+                },
+                {
+                  title:'文档CMS',
+                  collapsable: true,
+                  children:[
+                    '/guide/notes/two',
+                  ]
+                }
+            ]
+        },
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated',
+        searchMaxSuggestoins: 10,
+        serviceWorker: {
+            updatePopup: {
+                message: "有新的内容.",
+                buttonText: '更新'
+            }
+        },
+        editLinks: true,
+        editLinkText: '在 GitHub 上编辑此页 ！'
     }
 }
+
+
